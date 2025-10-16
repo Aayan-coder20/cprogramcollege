7889c751693bf625b883a7af86479caaafbd9d20
Aayan-coder20 <khnaayan07@gmail.com>
2025-10-16 14:33:49 +0530

    Add files via upload

--- second.c
+++ second.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 
 int main() {
-    int phy, chem, maths;
-    float avg;
-    printf("Enter marks of Physics: ");
-    scanf("%d", &phy);
-    printf("Enter marks of Chemistry: ");
-    scanf("%d", &chem);
-    printf("Enter marks of Mathematics: ");
-    scanf("%d", &maths);
-    avg = (phy + chem + maths) / 3.0;
-    printf("Average marks: %f \n", avg);
 
-    (avg >= 50) ? printf("Eligible for admission\n") : printf("Not Eligible for admission\n");
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) 
+        printf("%d is an even number \n", num);
+
+        else 
+        printf("%d is an odd number \n", num);
 
         return 0;
-
+    
 }
